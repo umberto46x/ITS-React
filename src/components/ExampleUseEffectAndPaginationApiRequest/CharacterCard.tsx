696ab5d60bea65ef23972851b1f44a84b08b4701
umberto46x <daniele.u.nicosia.dev@gmail.com>
2025-05-25 09:25:19 +0200

    exercise RickAndMortyAPI final commit

--- src/components/ExampleUseEffectAndPaginationApiRequest/CharacterCard.tsx
+++ src/components/ExampleUseEffectAndPaginationApiRequest/CharacterCard.tsx
@@ -7,52 +7,36 @@ type CharacterCardProps = {
 export const CharacterCard = (props: CharacterCardProps) => {
   return (
     <>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mx-auto">
+      <div className="w-75 h-100 bg-green-400 border border-black border-3 rounded-lg shadow-sm  mx-auto">
         <a href={props.character.url}>
           <img
-            className="rounded-t-lg mx-auto mt-5 size-48"
+            className="rounded-t-lg mx-auto mt-5 size-48 border-black border-3"
             src={props.character.image}
             alt={props.character.name}
           />
         </a>
-        <p>
-          {' '}
-          {props.character.gender}
-          {props.character.status}
-          {props.character.species}
-          {props.character.location.name}
-          {props.character.id}
-        </p>
+
         <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-black text-center">
             {props.character.name}
           </h5>
 
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+          <span className="mb-3 pr-3 text-xl font-normal text-black ">
+            {props.character.id}
+          </span>
+          <span className="mb-3 pr-3  text-xl font-normal text-black ">
+            {props.character.gender}
+          </span>
+          <span className="mb-3 pr-3  text-xl font-normal text-black ">
+            {props.character.status}
+          </span>
+          <span className="mb-3 pr-3  text-xl font-normal text-black">
+            {props.character.species}
+          </span>
+          <br />
+          <span className=" mb-3 pr-3  text-xl font-normal text-black">
+            {props.character.location.name}
+          </span>
         </div>
       </div>
     </>
--- src/components/ExampleUseEffectAndPaginationApiRequest/ExampleWithPagination.tsx
+++ src/components/ExampleUseEffectAndPaginationApiRequest/ExampleWithPagination.tsx
@@ -4,14 +4,6 @@ import { Loader } from './Loader';
 import { CharacterCard } from './CharacterCard';
 
 export const ExampleWithPagination = () => {
-  /* Chiamata http rickandmorty api che restituisce Characters da inserire in
-   un array di characters, successivamente inseriti singolarmente in dei componenti 
-   attraverso un map, aggiungere due buttons che cambiano la page della chiamata http,
-    extra disabilitare i button previous e next quando la variabile di stato page è minore 
-    di 1 o maggiore del massimo valore di page (all’interno della risposta.info)
-    extra: loading screen 
- */
-
   const [page, setPage] = useState<number>(1);
   const [characters, totalPages, isLoading] = useCharacters(page);
 
@@ -19,24 +11,24 @@ export const ExampleWithPagination = () => {
     <>
       <div className="grid mt-5 ">
         <div className="mx-auto mt-5">
-          <h2 className="p-5 text-center bg-green-400 mb-5 w-50 ">
+          <h2 className="p-5 text-center border border-black border-3 rounded-lg bg-green-400 mb-5 w-50 ">
             RickAndMorty API Exercise
           </h2>
         </div>
         <div className="mx-auto mt-5">
           <button
             className="bg-green-400 
-              rounded-4xl mx-5 d-block p-5"
+              border border-black border-3 rounded-lg mx-5 d-block p-5"
             disabled={page === 1}
             onClick={() => setPage(page - 1)}
           >
             Previous Page
           </button>
-          <span className="bg-green-400 rounded-4xl mx-5 d-block p-5 ">
+          <span className="bg-green-400 border border-black border-3 rounded-lg mx-5 d-block p-5 ">
             {page}
           </span>
           <button
-            className="bg-green-400   rounded-4xl mx-5 d-block p-5 "
+            className="bg-green-400   border border-black border-3 rounded-lg mx-5 d-block p-5 "
             disabled={page === totalPages}
             onClick={() => setPage(page + 1)}
           >
